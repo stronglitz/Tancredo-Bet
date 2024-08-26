c561eb54896fa7939348a753a2b86d1539397744
stronglitz <161094914+stronglitz@users.noreply.github.com>
2024-08-26 08:18:44 -0300

    ok

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function placeBet() {
-    const brasilBet = document.getElementById('bet-brasil').value;
-    const argentinaBet = document.getElementById('bet-argentina').value;
+function placeBet(team1, team2) {
+    const bet1 = document.getElementById(`bet-${team1.toLowerCase()}`).value;
+    const bet2 = document.getElementById(`bet-${team2.toLowerCase()}`).value;
     const resultElement = document.getElementById('bet-result');
 
-    if (brasilBet > argentinaBet) {
-        resultElement.innerHTML = '🎉 Parabéns! Você apostou na vitória do Brasil! 🇧🇷';
-    } else if (brasilBet < argentinaBet) {
-        resultElement.innerHTML = '🥳 Você apostou na vitória da Argentina! 🇦🇷';
+    if (bet1 > bet2) {
+        resultElement.innerHTML = `🎉 Parabéns! Você apostou na vitória do ${team1}!`;
+        resultElement.style.color = '#27ae60';
+    } else if (bet1 < bet2) {
+        resultElement.innerHTML = `🥳 Você apostou na vitória do ${team2}!`;
+        resultElement.style.color = '#f1c40f';
     } else {
-        resultElement.innerHTML = '🤔 Empate? Vamos ver no que dá! ⚽';
+        resultElement.innerHTML = `🤔 Empate? Vamos ver no que dá! ⚽`;
+        resultElement.style.color = '#e67e22';
     }
 }
